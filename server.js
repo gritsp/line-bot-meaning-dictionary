449aaccaf8ba13eb098b387dc6188fa729aa054c
Gritsanapong Punti <gritsanapongp@gmail.com>
2023-07-09 16:30:45 +0700

    fix bug and new text template

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const https = require("https");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,45 +16,37 @@ app.get("/", (req, res) => {
 });
 
 app.post("/webhook", async (req, res) =>{
-  res.send("HTTP POST request sent to the webhook URL!");
   if (req.body.events[0].type === "message") {
-    const text = req.body.events[0].message.text
-    const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${text}`)
-    const dataString = JSON.stringify({
-      replyToken: req.body.events[0].replyToken,
-      messages: [
-        {
-          type: "text",
-          text: JSON.stringify(data.data[0])
-        }
-      ],
-    });
-
     const headers = {
       "Content-Type": "application/json",
-      Authorization: "Bearer " + TOKEN,
+      "Authorization": "Bearer " + TOKEN,
     };
-
-    const webhookOptions = {
-      hostname: "api.line.me",
-      path: "/v2/bot/message/reply",
-      method: "POST",
-      headers: headers,
-      body: dataString,
-    };
-
-    const request = https.request(webhookOptions, (res) => {
-      res.on("data", (d) => {
-        process.stdout.write(d);
-      });
-    });
-
-    request.on("error", (err) => {
-      console.error(err);
-    });
-
-    request.write(dataString);
-    request.end();
+    const replyToken = req.body.events[0].replyToken
+    const text = req.body.events[0].message.text
+    axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${text}`)
+      .then(async (response) => {
+        const wordMeaning = response.data[0].meanings.map((item) => {
+          return { text:`${item.partOfSpeech}: ${item.definitions[0].definition}`,type:"text"}
+        })
+        const dataString = JSON.stringify({
+          replyToken,
+          messages: wordMeaning
+        });
+        axios.post(`https://api.line.me/v2/bot/message/reply`,dataString,{headers})
+        .then((response) => console.log(response.status===200?"success":"fail"))
+        .catch((err) => console.log(err))
+      }).catch((err) => {
+        const dataString = JSON.stringify({
+          replyToken,
+          messages: [{
+            text: "No Definitions Found",
+            type: "text"
+          }]
+        });
+        axios.post(`https://api.line.me/v2/bot/message/reply`,dataString,{headers})
+        .then((response) => console.log(`${text}: No Definitions Found`))
+        .catch((err) => console.log(err))
+      })
   }
 });
 
